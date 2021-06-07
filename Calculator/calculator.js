2317340b88fad3a694415b99a0040d409c589215
G7-WHJR <raveen.anand@whitehatjr.com>
2021-06-07 18:17:32 +0530

    FIRST

---
+++ Calculator/calculator.js
@@ -0,0 +1,17 @@
+/**Function add(): adds two numbers from the input and displays the answer */
+function add() {
+
+    //take number 1 from input box "n1" and store it in a variable named "input1"
+    
+
+     //take number 1 from input box "n1" and store it in a variable named "input1"
+    
+
+    //add input 1 and input 2 and store it in a variable named "result"
+    
+    // display the result in an HTML element with id "answer" 
+    
+}
+
+    
+    
